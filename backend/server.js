0c126ec20328bd2f380241ccfcbcdbec3b2c5a5f
styhrd <juanmiguelcaparas76@gmail.com>
2024-08-17 16:12:10 +0800

    MongoDB Connection

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
 import express from "express";
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 
+
+dotenv.config();
+connectDB()
 const app = express();
 
+const PORT = process.env.PORT || 8080 
+const STR = process.env.STRING || 'string' 
 
 //routes
 
@@ -9,6 +16,6 @@ app.get('/', (req, res) => {
     res.send("<h1>Welcome to Buhat</h1>")
 })
 
-app.listen(8080, () => {
-    console.log("Node Server is Running")
+app.listen(PORT, () => {
+    console.log(`Node Server is Running on ${PORT} ${STR}`)
 })
