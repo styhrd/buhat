9ec7f02d0995c6be527404cdef629b77cc64a76b
styhrd <juanmiguelcaparas76@gmail.com>
2024-09-30 21:11:23 +0800

    Creating test routes

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,25 @@
 import express from "express";
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import testRoutes from './routes/testRoutes.js'
+import cors from 'cors'
+import morgan from 'morgan'
 
 
 dotenv.config();
 connectDB()
 const app = express();
 
-const PORT = process.env.PORT || 8080 
-const STR = process.env.STRING || 'string' 
+const PORT = process.env.PORT || 8080
+const STR = process.env.STRING || 'string'
 
 //routes
 
-app.get('/', (req, res) => {
-    res.send("<h1>Welcome to Buhat</h1>")
-})
+app.use(express.json())
+app.use(cors())
+app.use(morgan('dev'))
+app.use("/api/v1/test", testRoutes)
+
 
 app.listen(PORT, () => {
     console.log(`Node Server is Running on ${PORT} ${STR}`)
