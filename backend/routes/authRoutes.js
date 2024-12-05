8039a26a6a12c6c6d92e5d9628f142595d12a20b
styhrd <juanmiguelcaparas76@gmail.com>
2024-12-05 17:38:12 +0800

    Adding Auth Middleware

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import { loginController, registerController } from '../controllers/authController.js'
+import userAuth from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
